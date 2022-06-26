1afd3c0e1632713fbadf25a66ae458d6b6576033
Antony Schmidt <antonyjonah@gmail.com>
2022-06-26 20:10:35 +0200

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-
+//router
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//pages & components
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+//styles
 import './index.css';
 
 function App() {
   return (
     <div className="App">
 
+
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
