8d18d9c90bdd36ddaa4055d8d574206267ac746a
Antony Schmidt <antonyjonah@gmail.com>
2022-06-27 20:07:52 +0200

    create config.js - setup firebase

---
+++ config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDzGhfbZON2wbYuHytbPurhxNUvPqaE4Ek",
+    authDomain: "mymoney-app-4c351.firebaseapp.com",
+    projectId: "mymoney-app-4c351",
+    storageBucket: "mymoney-app-4c351.appspot.com",
+    messagingSenderId: "935781222505",
+    appId: "1:935781222505:web:a5706343b28158009b7af8"
+};
+
+//init firebase
+
+firebase.initializeApp(firebaseConfig)
+
+// init service
+
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
