473454526bbe14ce7a5a2029edbc275848c0dc51
Antony Schmidt <antonyjonah@gmail.com>
2022-06-27 19:51:54 +0200

    create form for signup & login + change to ReactDom.render instead

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,46 @@
+import { useState } from 'react'
+//styles
 import styles from './Signup.module.css'
 
 export default function Signup() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+
+    }
+
     return (
-        <div>Signup</div>
+        <form className={styles['signup-form']} onSubmit={handleSubmit}>
+            <h2>Signup</h2>
+            <label>
+                <span>Name:</span>
+                <input
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+            </label>
+            <label>
+                <span>Email:</span>
+                <input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className='btn'>Signup</button>
+        </form>
     )
 }
