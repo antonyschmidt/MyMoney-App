0ef6146f3b5d40fe463cd80aded90ad6bfbbeea0
Antony Schmidt <antonyjonah@gmail.com>
2022-07-11 17:32:35 +0200

    add TransactionList.js, create and connect useCollection hook to fetch transactions, update useFirestore hook and Home component + styles

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { projectFirestore } from '../firebase/config'
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query, _order) => {
     const [documents, setDocuments] = useState(null)
     const [error, setError] = useState(null)
 
+
+    // if we dont use a useRef infinite loop is going to happen
+    // bcs _query is an array 
+    const query = useRef(_query).current
+    const order = useRef(_order).current
+
     useEffect(() => {
         let ref = projectFirestore.collection(collection)
+
+        if (query) {
+            ref = ref.where(...query)
+        }
+        if (order) {
+            ref = ref.orderBy(...order)
+        }
 
         const unsubscribe = ref.onSnapshot((snapshot) => {
             let results = []
@@ -26,7 +39,7 @@ export const useCollection = (collection) => {
 
         return () => unsubscribe()
 
-    }, [collection])
+    }, [collection, query, order])
 
     return { documents, error }
 }
